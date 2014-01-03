1ca2b95486efe978d99b7ce0427dbee867c742d9
Mattias Appelgren <mattias@ppelgren.se>
2014-01-03 23:24:49 +0100

    linting and moving things around

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 [![Build Status](https://travis-ci.org/Mattias-/xor.png?branch=master)](https://travis-ci.org/Mattias-/xor)
 [![Build Status](https://drone.io/github.com/Mattias-/xor/status.png)](https://drone.io/github.com/Mattias-/xor/latest)
 [![Coverage Status](https://coveralls.io/repos/Mattias-/xor/badge.png)](https://coveralls.io/r/Mattias-/xor)
+[![Code Health](https://landscape.io/github/Mattias-/xor/master/landscape.png)](https://landscape.io/github/Mattias-/xor/master)
 
 
 This is xor
--- xor/xor.py
+++ xor/xor.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-import subprocess as subp
-from urllib2 import unquote
 import json
 import os
 import signal
+import subprocess
+from urllib2 import unquote
 
-from flask import Flask, request, Response, stream_with_context, g
+import flask
 from werkzeug.serving import WSGIRequestHandler
 
 
@@ -15,125 +15,35 @@ DEBUG = True
 
 class XorRequestHandler(WSGIRequestHandler):
     def connection_dropped(self, error, environ=None):
-        if g.proc:
-            print "Killing process: ", g.proc
-            os.killpg(g.proc.pid, signal.SIGKILL)
-            g.proc.communicate()
+        if flask.g.proc:
+            process = flask.g.proc
+            print "Killing process: ", process
+            os.killpg(process.pid, signal.SIGKILL)
+            process.communicate()
 
 
 def main(argv):
     config_rules = json.load(open(argv[1]))
-    x = Xor()
-    x.add_rules(config_rules)
-    x.app.run(host='0.0.0.0', debug=DEBUG, extra_files=[argv[1]],
-              request_handler=XorRequestHandler)
+    xor = Xor()
+    xor.add_rules(config_rules)
+    xor.app.run(host='0.0.0.0', debug=DEBUG, extra_files=[argv[1]],
+                request_handler=XorRequestHandler)
 
 
-def read_generator(fd, b):
-    return iter(lambda: fd.read(b), '')
+def read_generator(file_d, size):
+    return iter(lambda: file_d.read(size), '')
 
 
-def run_cmd(cmd, user=None, output=False, script=False):
-    if user:
-        running_user = subp.check_output(['whoami']).strip()
-        if user != running_user:
-            if running_user == 'root':
-                cmd = ['sudo', '-u', user] + cmd
-            else:
-                raise Exception('Permission denied')
-    if output:
-        p = subp.Popen(cmd, stdout=subp.PIPE, stderr=subp.STDOUT,
-                       bufsize=2, preexec_fn=os.setsid, shell=(not script))
-        g.proc = p
-        return Response(stream_with_context(read_generator(p.stdout, 1)))
-    else:
-        try:
-            p = subp.Popen(cmd, stdout=subp.PIPE, stderr=subp.STDOUT,
-                           preexec_fn=os.setsid, shell=(not script))
-            (a, b) = p.communicate()
-            exit_value = p.returncode
-        except OSError as e:
-            print e
-            exit_value = e.errno
-        return str(exit_value)
-
-
-def get_path_args(kwargs, route):
-    def get_vars(route):
-        res = []
-        vs = route.split('/')
-        for v in vs:
-            if v.startswith('<') and v.endswith('>'):
-                v = v[1:-1]
-                ind = v.find(':')
-                res.append(v[ind+1:])
-        return res
-    return [unicode(kwargs[x]) for x in get_vars(route)]
-
-
-def get_query_args(request):
-    if len(request.query_string) > 0:
-        query_string = unquote(request.query_string)
-        return query_string.split('&')
-    else:
-        return []
-
-
-def get_post_args(request):
-    if request.method == 'POST':
-        content = request.environ['wsgi.input']
-        post_data = unquote(content.read(request.content_length))
-        return post_data.split('&')
-    else:
-        return []
-
-
-def order_args(order, args):
-    arg_list = []
-    for o in order + ['query', 'path', 'post']:
-        if o in args:
-            arg_list.extend(args[o])
-            del args[o]
-    return arg_list
-
-
-# Function factory, because of late binding.
-def create_view(route=None, output=False, order=None, user=None, script=None,
-                command=None, **options):
-    if not route:
-        raise Exception
-    if not order:
-        order = ['query', 'path', 'post']
-
-    def view(**kwargs):
-        args = {}
-        args['path'] = get_path_args(kwargs, route)
-        args['query'] = get_query_args(request)
-        args['post'] = get_post_args(request)
-        arg_list = order_args(order, args)
-        if script:
-            this_cmd = [script] + arg_list
-        else:
-            arg_list = safe_args(arg_list)
-            this_cmd = [' '.join([command] + arg_list)]
-        if DEBUG:
-            print this_cmd
-        return run_cmd(this_cmd, user=user, output=output, script=script)
-    return view
-
-def safe_args(li):
-    chars = ';>|&'
-    res = []
-    for arg in li:
-        for c in chars:
-            arg = arg.replace(c, '\\%s' % c)
-        res.append(arg)
-    return res
+def escape_arg(arg):
+    chars = [';', '>', '|', '&']
+    for char in chars:
+        arg = arg.replace(char, '\\%s' % char)
+    return arg
 
 
 class Xor(object):
     def __init__(self):
-        self.app = Flask(__name__)
+        self.app = flask.Flask(__name__)
 
     def add_rules(self, cfg):
         for rule in cfg:
@@ -142,9 +52,104 @@ class Xor(object):
             if 'script' in rule or 'command' in rule:
                 endpoint = 'f%s' % str(hash(rule['route']))
                 self.app.add_url_rule(rule['route'], endpoint,
-                                      create_view(**rule),
+                                      Xor.__create_view(**rule),
                                       methods=rule.get('methods'),
                                       defaults=rule.get('defaults'))
+
+    @staticmethod
+    def __run_cmd(cmd, user=None, output=False, script=False):
+        if user:
+            running_user = subprocess.check_output(['whoami']).strip()
+            if user != running_user:
+                if running_user == 'root':
+                    cmd = ['sudo', '-u', user] + cmd
+                else:
+                    raise Exception('Permission denied')
+        if output:
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                       stderr=subprocess.STDOUT,
+                                       bufsize=2, preexec_fn=os.setsid,
+                                       shell=(not script))
+            flask.g.proc = process
+            stream = read_generator(process.stdout, 1)
+            return flask.Response(flask.stream_with_context(stream))
+        else:
+            try:
+                process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                           stderr=subprocess.STDOUT,
+                                           preexec_fn=os.setsid,
+                                           shell=(not script))
+                process.communicate()
+                exit_value = process.returncode
+            except OSError as error:
+                exit_value = error.errno
+            return flask.make_response(str(exit_value))
+
+    # Function factory, because of late binding.
+    @staticmethod
+    def __create_view(route=None, output=False, order=None, user=None,
+                      script=None, command=None, **options):
+        if not route:
+            raise Exception
+        if not order:
+            order = ['query', 'path', 'post']
+
+        def view(**kwargs):
+            args = {}
+            args['path'] = Xor.__get_path_args(kwargs, route)
+            args['query'] = Xor.__get_query_args(flask.request)
+            args['post'] = Xor.__get_post_args(flask.request)
+            arg_list = Xor.__order_args(order, args)
+            if script:
+                this_cmd = [script] + arg_list
+            else:
+                arg_list = map(escape_arg, arg_list)
+                this_cmd = [' '.join([command] + arg_list)]
+            if DEBUG:
+                print this_cmd
+            return Xor.__run_cmd(this_cmd, user=user, output=output,
+                                 script=script)
+        return view
+
+    @staticmethod
+    def __get_path_args(kwargs, route):
+        def get_vars(route):
+            res = []
+            route_vars = route.split('/')
+            for var in route_vars:
+                if var.startswith('<') and var.endswith('>'):
+                    var = var[1:-1]
+                    index = var.find(':')
+                    res.append(var[index+1:])
+            return res
+        return [unicode(kwargs[x]) for x in get_vars(route)]
+
+    @staticmethod
+    def __get_query_args(req):
+        if len(req.query_string) > 0:
+            query_string = unquote(req.query_string)
+            return query_string.split('&')
+        else:
+            return []
+
+    @staticmethod
+    def __get_post_args(req):
+        if req.method == 'POST':
+            content = req.environ['wsgi.input']
+            post_data = unquote(content.read(req.content_length))
+            return post_data.split('&')
+        else:
+            return []
+
+    @staticmethod
+    def __order_args(order, args):
+        arg_list = []
+        for arg_group in order + ['query', 'path', 'post']:
+            if arg_group in args:
+                arg_list.extend(args[arg_group])
+                del args[arg_group]
+        return arg_list
+
 
 if __name__ == '__main__':
     import sys
